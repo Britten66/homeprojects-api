d6bb7ec8e7a830ad462b703e92bd8213d764bdc2
Chris <Britten63@hotmail.com>
2026-06-18 20:24:23 -0300

    added mapping and imports

--- src/main/java/homeprojects/HomeProjectController.java
+++ src/main/java/homeprojects/HomeProjectController.java
@@ -1,4 +1,21 @@
+package homeprojects;
+
+
+
 import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
 
 @RestController
 @RequestMapping("/api/projects")
@@ -10,4 +27,14 @@ public class HomeProjectController {
     @GetMapping
     public List<HomeProject> getAll() {
         return service.getAll();
+    }
+
+
+    @GetMapping("/{id}")
+    public ResponseEntity<HomeProject> getById(@PathVariable Long id) {
+        HomeProject project = service.getById(id);
+        if (project == null) {
+            return ResponseEntity.notFound().build();
+        }
+        return ResponseEntity.ok(project);
     }
